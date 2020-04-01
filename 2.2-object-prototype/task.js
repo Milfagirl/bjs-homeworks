cb7fa5e7d938a4fb8dae9b7dca4ed5eeec4d165d
Svetlana Ryabova <ganyim@inbox.ru>
2020-04-02 00:48:10 +0600

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,34 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    // let sound;
+           
+    if(typeof animal == "undefined") {
+        return null
+    } 
+    sound = animal.sound;
+    
+    if(typeof animal !== "undefined") {
+        return sound;
+    } 
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let average = 0;
+    let value = 0;
+    let roundedAverage;
+    for (let i=0; i< marks.length; i++) {   //перебираем массив
+        value = Number(marks[i]) + value;    //оценки переводим в числовой тип и суммируем
+    }
+    average = value / marks.length;
+    roundedAverage = Math.round(average);
+    return roundedAverage
 }
 
 function checkBirthday(birthday) {
+
+    // Делала в предыдущем домашнем задании
+
+
+
     // код для задачи №3 писать здесь
     // return verdict
 }
